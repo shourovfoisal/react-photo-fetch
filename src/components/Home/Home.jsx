28e65592da0c12063f17960caf829a7162fcafac
Shourov Foisal <shourovfoisal@gmail.com>
2022-09-24 17:10:42 +0600

    Added a scroll to top button to the bottom right of the page.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import Gallery from "../Gallery/Gallery";
 import Nav from "../Nav/Nav";
 import ExpandOverlay from "../ExpandOverlay/ExpandOverlay";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
 
 export default function Home() {
   const items = useSelector((state) => state.images.fetchedImages);
@@ -19,6 +20,7 @@ export default function Home() {
         <div className="noImages textDark">Please select an item.</div>
       )}
       {imageLink ? <ExpandOverlay image={imageLink} alt={imageAlt} /> : null}
+      <ScrollToTop />
     </div>
   );
 }
---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,28 @@
+import "./ScrollToTop.css";
+
+const ScrollToTop = () => {
+  const scrollButton = document.getElementById("scrollToTop");
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 200 ||
+      document.documentElement.scrollTop > 200
+    ) {
+      scrollButton.setAttribute("style", "opacity: 1");
+    } else {
+      scrollButton.setAttribute("style", "opacity: 0");
+    }
+  }
+  const handleScroll = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  return (
+    <div id="scrollToTop" onClick={() => handleScroll()}>
+      <ion-icon name="chevron-up-outline"></ion-icon>
+    </div>
+  );
+};
+
+export default ScrollToTop;
